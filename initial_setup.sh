8cdbcd15c7b22359bb5f9c70b646647dd6ea9757
Jeremy Pinto <pintojer@eos13.iro.umontreal.ca>
2018-12-10 12:55:10 -0500

    Update initial setup to save data in /Tmp

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,10 +1,12 @@
-if [ ! -d data/SVHN/train]; then
+DATA_DIR='/Tmp/'$USER'/data/SVHN'
+mkdir -p $DATA_DIR
+
+if [ ! -d $DATA_DIR/train ]; then
     echo "Downloading files for the training set!"
-    wget -P ./data/SVHN/ http://ufldl.stanford.edu/housenumbers/train.tar.gz
-    tar -xzf ./data/SVHN/train.tar.gz -C ./data/SVHN/
+    wget -P $DATA_DIR http://ufldl.stanford.edu/housenumbers/train.tar.gz
+    tar -xzf $DATA_DIR/train.tar.gz -C $DATA_DIR
     echo "Download finished, cleaning up..." 
-    rm ./data/SVHN/train.tar.gz
+    rm $DATA_DIR/train.tar.gz
 else
     echo "Train files already present"
 fi
-
