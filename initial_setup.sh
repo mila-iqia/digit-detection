7c39aafa62b2e0d52acf93d460ff893c7391fa8b
Jeremy Pinto <pintojer@elisa1.iro.umontreal.ca>
2018-12-11 11:50:16 -0500

    Use old conda syntax

--- initial_setup.sh
+++ initial_setup.sh
@@ -10,18 +10,17 @@ if [ ! -d $TMP_DATA_DIR/train ]; then
     echo "Downloading files for the training set!"
     wget -P $TMP_DATA_DIR http://ufldl.stanford.edu/housenumbers/train.tar.gz
     tar -xzf $TMP_DATA_DIR/train.tar.gz -C $TMP_DATA_DIR
-    echo "Download finished, cleaning up..." 
+    echo "Download finished, cleaning up..."
     rm $TMP_DATA_DIR/train.tar.gz
 else
     echo "Train files already present"
 fi
-. /home/jerpint/miniconda3/etc/profile.d/conda.sh
-conda env create -f $ROOT_DIR'environment.yml'
-conda activate digit-detection
+
+conda env create -f $ROOT_DIR'/environment.yml'
+source activate digit-detection
 python train.py
 
 echo "Copying files to local hard drive..."
-echo $TMP_RESULTS_DIR
 cp -r $TMP_RESULTS_DIR $ROOT_DIR
 
 echo "Cleaning up data..."
