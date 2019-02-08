c3f72f32b78ce35f40d6e8c4aa3c720b307c78b7
Jeremy Pinto <jerpint@gmail.com>
2019-02-08 11:32:47 -0500

    Add model with dropout

--- models/models.py
+++ models/models.py
@@ -61,7 +61,7 @@ class ConvNet(nn.Module):
         return out
 
 
-class BaselineCNN(nn.Module):
+class BaselineCNN(nn.Module): # Achieves ~91%
 
     def __init__(self):
         '''
@@ -99,6 +99,55 @@ class BaselineCNN(nn.Module):
         x = x.view(x.size(0), -1)
 
         x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
+
+
+class BaselineCNN_dropout(nn.Module):
+
+    def __init__(self, p=0.5):
+        '''
+        Placeholder CNN
+        '''
+        super(BaselineCNN_dropout, self).__init__()
+
+        self.p = p
+        self.conv1 = nn.Conv2d(3, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(self.p)
+
+        self.fc1 = nn.Linear(7744, 4096)
+        self.fc2 = nn.Linear(4096, 7)
+
+    def forward(self, x):
+        '''
+        Forward path.
+
+        Parameters
+        ----------
+        x : ndarray
+            Input to the network.
+
+        Returns
+        -------
+        x : ndarray
+            Output to the network.
+
+        '''
+
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.dropout(x)
+
+        x = self.pool(F.relu(self.conv2(x)))
+        x  = self.dropout(x)
+        # Flatten based on batch size
+        x = x.view(x.size(0), -1)
+
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
 
         return x
