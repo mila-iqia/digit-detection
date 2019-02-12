0185c3606553b834b0692f2abf5464d001107840
Jeremy Pinto <jerpint@gmail.com>
2019-02-12 12:30:32 -0500

    Use vgg 19

--- train.py
+++ train.py
@@ -136,15 +136,15 @@ if __name__ == '__main__':
 
     # Define model architecture
     # baseline_cnn = ConvNet(num_classes=7)
-    baseline_cnn = BaselineCNN(num_classes=7)
-    resnet18 = ResNet18(num_classes=7)
-    #  vgg = VGG('VGG11', num_classes=7)
+    # baseline_cnn = BaselineCNN(num_classes=7)
+    # resnet18 = ResNet18(num_classes=7)
+    vgg19 = VGG('VGG19', num_classes=7)
     # baseline_cnn = BaselineCNN_dropout(num_classes=7, p=0.5)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("Device used: ", device)
 
-    train_model(resnet18,
+    train_model(vgg19,
                 train_loader=train_loader,
                 valid_loader=valid_loader,
                 num_epochs=cfg.TRAIN.NUM_EPOCHS,
