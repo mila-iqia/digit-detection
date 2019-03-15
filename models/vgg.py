821d930d601c688112a2850d71993d5631517fcc
Museau <margaux.luck@gmail.com>
2019-03-15 11:45:44 -0400

    add comments to models/vgg.py

--- models/vgg.py
+++ models/vgg.py
@@ -2,20 +2,36 @@
 
 Source: https://github.com/kuangliu/pytorch-cifar/blob/master/models/vgg.py
 '''
+
 import torch
 import torch.nn as nn
 
 
 cfg = {
-    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M',
+              512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256,
+              'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256,
+              'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512,
+              512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
 }
 
 
 class VGG(nn.Module):
     def __init__(self, vgg_name, num_classes=7, classify=True):
+        '''
+        VGG11/13/16/19 model.
+
+        Parameters
+        ----------
+        vgg_name : str
+            The type of VGG. In ['VGG11', 'VGG13', 'VGG16', 'VGG19']
+        num_classes : int
+            Number of classes in the output of the model.
+
+        '''
         super(VGG, self).__init__()
         self.features = self._make_layers(cfg[vgg_name])
         self.classify = classify
@@ -24,6 +40,20 @@ class VGG(nn.Module):
             self.classifier = nn.Linear(512, num_classes)
 
     def forward(self, x):
+        '''
+        Forward path.
+
+        Parameters
+        ----------
+        x : ndarray
+            Input to the network.
+
+        Returns
+        -------
+        x : ndarray
+            Output to the network.
+
+        '''
         out = self.features(x)
         out = out.view(out.size(0), -1)
         if self.classify:
@@ -31,6 +61,16 @@ class VGG(nn.Module):
         return out
 
     def _make_layers(self, cfg):
+        '''
+        Helper function to make layers in the model.
+
+        Parameters
+        ----------
+        cfg : list
+            list of layer type and output size. M is for MaxPooling and
+            int are for output size.
+
+        '''
         layers = []
         in_channels = 3
         for x in cfg:
@@ -46,6 +86,7 @@ class VGG(nn.Module):
 
 
 def test():
+    '''Test the VGG class.'''
     net = VGG('VGG11', num_classes=7)
     x = torch.randn(2, 3, 32, 32)
     y = net(x)
