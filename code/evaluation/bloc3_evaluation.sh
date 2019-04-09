1a5d16ad2c1ce4db1f0c191901be2bf89bb19f92
Jeremy Pinto <jerpint@gmail.com>
2019-04-09 11:32:10 -0400

    Add evaluation script

---
+++ code/evaluation/bloc3_evaluation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the conda environment
+source activate humanware
+
+### FILL OUT THE REST OF THE FILE ###
+
+# You should build maskrcnn-bechnmark
+# The final results should be saved to $RESULTS_DIR
+# Refer to evaluation_instructions.md for more information
